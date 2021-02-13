f95ecb83121a1194f2dbdce65683bf42a661f7b9
Andrew Marks <andrewmarks@Andrews-Mac-Mini.local>
2021-02-13 20:30:58 +0000

    Updates

---
+++ precache-manifest.39741a2c5645ae57efa802863db1ed53.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eae162678778d1b568ebb5ddceb0cbd",
+    "url": "/emoji-dictionary/index.html"
+  },
+  {
+    "revision": "af27332e92293defcd14",
+    "url": "/emoji-dictionary/static/js/2.2a3af9a5.chunk.js"
+  },
+  {
+    "revision": "0b6b1fcad76f686aa501",
+    "url": "/emoji-dictionary/static/js/main.98ab152d.chunk.js"
+  },
+  {
+    "revision": "2f21852cac36fe0dba91",
+    "url": "/emoji-dictionary/static/js/runtime-main.7f4588e1.js"
+  }
+]);
